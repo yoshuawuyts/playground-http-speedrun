8fc4e47c810f8efaa3c783b7dddedcf9a184e55d
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-08-10 19:17:50 +0200

    .

--- http_01/src/main.rs
+++ http_01/src/main.rs
@@ -3,13 +3,37 @@
 #![cfg_attr(feature = "nightly", doc(include = "../README.md"))]
 #![cfg_attr(test, deny(warnings))]
 
-extern crate structopt;
+extern crate clap_flags;
+extern crate futures;
 extern crate http_01;
+extern crate hyper;
+extern crate structopt;
+extern crate tokio;
+#[macro_use]
+extern crate log;
 
+use futures::prelude::*;
 use http_01::cli::Cli;
+use hyper::service::service_fn_ok;
+use hyper::{Body, Response, Server};
 use structopt::StructOpt;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-  let _args = Cli::from_args();
+  let args = Cli::from_args();
+  let listener = args.port.bind()?;
+
+  args.logger.log_all(args.verbosity.log_level())?;
+
+  let handle = tokio::reactor::Handle::current();
+  let listener = tokio::net::TcpListener::from_std(listener, &handle)?;
+  let addr = listener.local_addr()?;
+
+  let server = Server::builder(listener.incoming())
+    .serve(|| service_fn_ok(|_| Response::new(Body::from("Hello World"))))
+    .map_err(|e| eprintln!("server error: {}", e));
+
+  info!("Server listening on {}", addr);
+  tokio::run(server);
+
   Ok(())
 }
