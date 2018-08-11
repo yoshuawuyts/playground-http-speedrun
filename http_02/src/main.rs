b8083a88077738cb6cc9d2a74ffae8443caad0d2
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-08-11 16:08:48 +0200

    parse query

--- http_02/src/main.rs
+++ http_02/src/main.rs
@@ -3,13 +3,27 @@
 #![cfg_attr(feature = "nightly", doc(include = "../README.md"))]
 #![cfg_attr(test, deny(warnings))]
 
-extern crate structopt;
+extern crate argon2;
 extern crate http_02;
+extern crate structopt;
+#[macro_use]
+extern crate warp;
 
 use http_02::cli::Cli;
 use structopt::StructOpt;
+use warp::Filter;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
   let _args = Cli::from_args();
+
+  let login = warp::get(path!("login"))
+    .and(warp::filters::query::query())
+    .map(|query: String| format!("login: {}", query));
+
+  let signup = warp::get(path!("signup")).map(|| format!("Signing up"));
+
+  let server = warp::serve(login.or(signup));
+  println!("listening on port 8080");
+  server.run(([127, 0, 0, 1], 8080));
   Ok(())
 }
